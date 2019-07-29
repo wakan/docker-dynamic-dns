fa2851a170fe13cefad2bfa6438dfc2249abfd29
WaKaN <wakan_powa_s01@hotmail.fr>
2019-07-29 18:27:01 +0200

    Refacto in function + interval

--- pool.sh
+++ pool.sh
@@ -20,58 +20,64 @@ then
 fi
 
 
-if [ -n "$DETECTIP" ]
-then
-	IP=$(curl http://myexternalip.com/raw)
-fi
-
-
-if [ -n "$DETECTIP" ] && [ -z $IP ]
-then
-	RESULT="Could not detect external IP."
-	exit 42
-fi
-
-
 if [[ $INTERVAL != [0-9]* ]]
 then
 	echo "Interval is not an integer."
 	exit 35
 fi
-SERVICEURL="www.changeip.com/nic/update"
 
-BASE64AUTH=$(echo -n "$USER:$PASSWORD" | base64 | tr -d \\n)
-AUTHHEADER="Authorization: Basic $BASE64AUTH"
-NOIPURL="https://$SERVICEURL"
 
-if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
-then
-	NOIPURL="$NOIPURL?"
-fi
+checkip_and_update_ddns() {
+	if [ -n "$DETECTIP" ]
+	then
+		IP=$(curl http://myexternalip.com/raw)
+	fi
 
-if [ -n "$HOSTNAME" ]
-then
-	NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
-fi
 
-if [ -n "$IP" ]
-then
+	if [ -n "$DETECTIP" ] && [ -z $IP ]
+	then
+		RESULT="Could not detect external IP."
+		exit 42
+	fi
+
+	SERVICEURL="www.changeip.com/nic/update"
+
+	BASE64AUTH=$(echo -n "$USER:$PASSWORD" | base64 | tr -d \\n)
+	AUTHHEADER="Authorization: Basic $BASE64AUTH"
+	NOIPURL="https://$SERVICEURL"
+
+	if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
+	then
+		NOIPURL="$NOIPURL?"
+	fi
+
 	if [ -n "$HOSTNAME" ]
 	then
-		NOIPURL="$NOIPURL&"
+		NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
 	fi
-	NOIPURL="${NOIPURL}myip=$IP"
-fi
 
-echo "$NOIPURL  -H "$AUTHHEADER"  -H 'Connection: keep-alive'"
-
-RESULT=$(curl -X GET   $NOIPURL  -H "$AUTHHEADER"  -H 'Connection: keep-alive')
-echo $RESULT
-
-	if [ $INTERVAL -eq 0 ]
+	if [ -n "$IP" ]
 	then
-		break
-	else
-		sleep "${INTERVAL}m"
+		if [ -n "$HOSTNAME" ]
+		then
+			NOIPURL="$NOIPURL&"
+		fi
+		NOIPURL="${NOIPURL}myip=$IP"
 	fi
+
+	echo "$NOIPURL  -H "$AUTHHEADER"  -H 'Connection: keep-alive'"
+
+	RESULT=$(curl -X GET   $NOIPURL  -H "$AUTHHEADER"  -H 'Connection: keep-alive')
+	echo $RESULT
+	
+}
+
+if [ $INTERVAL -ne 0 ]
+then
+	while
+	do
+		checkip_and_update_ddns
+		sleep "${INTERVAL}m"
+	done
+fi
 exit 0	
